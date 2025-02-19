2612957b612e0b39847afbd5dcc86c701ce80f60
ravijar <ravijarandinu@gmail.com>
2025-02-19 16:07:39 +0530

    fixed name issue

--- api/serializers/user.py
+++ api/serializers/user.py
@@ -1,17 +1,19 @@
 from rest_framework import serializers
-from api.models.user import CustomUser
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
-        fields = ['id', 'name', 'email', 'password', 'address']
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'address']  # Replace 'name' with 'username'
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        """Create a new user with hashed password."""
-        user = CustomUser.objects.create_user(
+        """Create a new user and hash password securely."""
+        user = User.objects.create_user(
+            username=validated_data['username'],
             email=validated_data['email'],
-            name=validated_data['name'],
             password=validated_data['password'],
             address=validated_data.get('address', "")
         )
