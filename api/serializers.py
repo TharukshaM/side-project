e4c65c8460da893fac956e06e8e7d6d1cc48da4d
TharukshaM <fernandoatm.20@uom.lk>
2025-02-19 11:53:09 +0530

    remove cache

--- api/serializers.py
+++ api/serializers.py
@@ -1,18 +1,2 @@
-from rest_framework import serializers
-from .models import *
 
 # converts DJ models into json format for the REST API
-class FurnitureSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Furniture
-        fields = '__all__'
-
-class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = '__all__'
-
-class OrderItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OrderItem
-        fields = '__all__'
